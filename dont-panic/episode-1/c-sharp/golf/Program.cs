7ed1af5eb9ae0ff8e6dd847425d5b57ae07efff9
DouglasMarsh <marsh.douglas.e@gmail.com>
2022-04-07 09:23:57 -0500

    add dont-panic code

---
+++ dont-panic/episode-1/c-sharp/golf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+class D{
+static void Main(string[] args){
+Func<string,int> p=(string s)=>int.Parse(s);
+Func<string[]> r=()=>Console.ReadLine().Split();
+Func<int[]> y=()=>r().Select(i=>p(i)).ToArray();
+var l=y();var s=new int[l[0]];
+s[l[3]]=l[4];
+for(int i=0;i++<l[7];){ var m=y();s[m[0]]=m[1];}
+while(true){var d = r();
+int f=p(d[0]);int t=p(d[1]);var z=d[2][0];var c="WAIT";
+if(z!='N'&&s[f]!=t){var n=s[f]-t>0?'R':'L';if(n!=z&&t!=s[Math.Max(0,f-1)])c="BLOCK";}
+Console.WriteLine(c);}}}

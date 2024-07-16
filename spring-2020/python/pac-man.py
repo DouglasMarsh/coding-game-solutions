6f958c4fa82ba7a6a9f9865f19cc1d5e353110ac
douglasmarsh <marsh.douglas.e@gmail.com>
2024-07-16 13:33:15 -0500

    bronze 611

--- spring-2020/python/pac-man.py
+++ spring-2020/python/pac-man.py
@@ -2,30 +2,25 @@ import sys
 import math
 import random
 
-from collections import namedtuple
-from typing import Callable
-  
-Point = namedtuple("Point", "x y")
+from typing import NamedTuple, Union
+
+Point = NamedTuple("Point", [('x', int), ('y', int)])
 
 # Grab the pellets as fast as you can!
-class reversor:
-    def __init__(self, obj):
-        self.obj = obj
-
-    def __eq__(self, other):
-        return other.obj == self.obj
-
-    def __lt__(self, other):
-        return other.obj < self.obj
-
 class Pellet:
     def __init__(self, pos: Point, value: int) -> None:
         self.pos = pos
         self.value = value
-
+    def __repr__(self) -> str:
+        return f"Pellet{self.pos}:{self.value}"
+    def __hash__(self) -> int:
+        return hash( self.__repr__())
+    
 class PacMan:
     pos:Point
-    target:Pellet = None
+
+    target:Union[None,Point] = None    
+    point_target:Union[None,Point] = None
 
     type = ""
     boost = 0
@@ -33,19 +28,64 @@ class PacMan:
     def __init__(self, _id: int) -> None:
         self._id = _id
 
+def set_targets( pac_men: list[PacMan]):
+    global pellets
+    global big_pellets
+
+    # set targets    
+    if big_pellets or pellets:
+        targets: dict[Pellet, list[PacMan]] = {}
+        for pm in pac_men:
+            if not pm.target or pm.target not in pellets:
+                pm.target = None
+                if big_pellets:    
+                    pm.target = next((p for p in sorted(big_pellets, key=lambda p: math.dist(p.pos, pm.pos))), None)
+                elif pellets:    
+                    pm.target = next((p for p in sorted(pellets, key=lambda p: math.dist(p.pos, pm.pos))), None)
+
+            if pm.target:
+                targets.setdefault( pm.target, []).append( pm )
+
+        re_target: list[PacMan] = []
+        for t in targets:
+            pellets.remove( t )
+            if t in big_pellets:
+                big_pellets.remove( t )
+            
+            if len( targets[t]) > 1:
+                targets[t].sort(key=lambda pm: math.dist(pm.pos, t.pos ))
+                re_target += targets[t][1:]
+
+        if re_target:
+            set_targets( re_target )
+    else:
+        for pm in pac_men:
+            pm.target = None
+
+grid: dict[Point, str] = {}
+open_points: list[Point] = []
+
 # width: size of the grid
 # height: top left corner is (x=0, y=0)
 width, height = [int(i) for i in input().split()]
-for i in range(height):
+for y in range(height):
     row = input()  # one line of the grid: space " " is floor, pound "#" is wall
+    for x in range(width):
+        grid[Point(x,y)] = row[x]
+        if row[x] == " ":
+            open_points.append( Point(x,y))
 
-pac_men: dict[int, PacMan] = {}
-op_pac_men: dict[int, PacMan] = {}
+
+all_pac_men: dict[int, PacMan] = {}
 
 # game loop
 while True:
+    pac_men: list[PacMan] = []
+    op_pac_men: list[PacMan] = []
+
     pellets: list[ Pellet ] = [] 
     big_pellets: list[ Pellet ] = []
+
     my_score, opponent_score = [int(i) for i in input().split()]
     visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
     for i in range(visible_pac_count):
@@ -53,20 +93,21 @@ while True:
         # 0: pac number (unique within a team)
         # 1: true if this pac is yours
         # 2-3: position in the grid
-        # unused in wood leagues
-        # unused in wood leagues
-        # unused in wood leagues
+        # type: ROCK or PAPER or SCISSORS OR DEAD
+        # boost remaining: number of turns of boost (0-5)
+        # cooldown turns left (number of turns before Pac can boost or switch form)
 
         pac_id = int(inputs[0])  # pac number (unique within a team)
+        pm = all_pac_men.setdefault(pac_id, PacMan( pac_id ))
         mine = inputs[1] != "0"
         if mine:
-            pm = pac_men.setdefault(pac_id, PacMan( pac_id ))            
+            pac_men.append( pm )
             pm.pos = Point(int(inputs[2]), int(inputs[3]))
             pm.type = inputs[4]
             pm.boost = int(inputs[5])
             pm.cooldown = int(inputs[6])
         else:
-            pm = op_pac_men.setdefault(pac_id, PacMan( pac_id ))            
+            op_pac_men.append( pm )          
             pm.pos = Point(int(inputs[2]), int(inputs[3]))
             pm.type = inputs[4]
             pm.boost = int(inputs[5])
@@ -81,40 +122,51 @@ while True:
         if p.value == 10:
             big_pellets.append( p )
     
+
+    set_targets( pac_men )
+    
     output:list[str] = []
-    for pm in pac_men.values():
+    for pm in pac_men:
 
-        #handle collisions
-        if pm.pos in [op.pos for op in op_pac_men]:
-            pm.target = random.choice( pellets )
-
-        p = pm.target
-        
-        if p and p in pellets:
-            print(f"MOVE to pellet at {p.pos.x} {p.pos.y} with value {p.value} at dist { math.dist(p.pos, pm.pos) }", file=sys.stderr, flush=True)
-            output.append( f"MOVE {pm._id} {p.pos.x} {p.pos.y}" )
-            pellets.remove( p )
-            if p in big_pellets: big_pellets.remove( p )
-            continue
-
-        if len( big_pellets) > 0:
-            
-            big_pellets.sort(key = lambda p: math.dist(p.pos, pm.pos), reverse=True)
-            p = big_pellets.pop()
-            pellets.remove( p )
-            pm.target = p
-
-            print(f"MOVE to pellet at {p.pos.x} {p.pos.y} with value {p.value} at dist { math.dist(p.pos, pm.pos) }", file=sys.stderr, flush=True)
-            output.append( f"MOVE {pm._id} {p.pos.x} {p.pos.y}" )
+        if len([cp for cp in all_pac_men.values() if cp._id != pm._id and cp.pos == pm.pos]) > 0:
+            pt = random.choice( open_points )
+            print(f"Collision!! MOVE to random point at {pt.x} {pt.y}", file=sys.stderr, flush=True)
+            output.append(f"MOVE {pm._id} {pt.x} {pt.y}")
             continue
         
-        pellets.sort(key = lambda p: math.dist(p.pos, pm.pos), reverse=True)
-        p = pellets.pop()
-        pm.target = p
+        op = next((op for op in op_pac_men if math.dist(pm.pos, op.pos) == (2 if op.boost > 0 else 1) ), None)
+        if op and pm.cooldown == 0:
+            if op.type == "ROCK" and pm.type != "PAPER":
+                output.append(f"SWITCH {pm._id} PAPER")
+                continue
+            elif op.type == "PAPER" and pm.type != "SCISSORS":
+                output.append(f"SWITCH {pm._id} SCISSORS")
+                continue
+            elif op.type == "SCISSORS" and pm.type != "ROCK":
+                output.append(f"SWITCH {pm._id} ROCK")
+                continue
         
-        print(f"MOVE to pellet at {p.pos.x} {p.pos.y} with value {p.value} at dist { math.dist(p.pos, pm.pos) }", file=sys.stderr, flush=True)
-        output.append(f"MOVE {pm._id} {p.pos.x} {p.pos.y}")
+        op = next((op for op in op_pac_men if math.dist(pm.pos, op.pos) < 5+op.boost), None)
+        if not op and pm.cooldown == 0 and pm.boost == 0:
+            output.append(f"SPEED {pm._id}")
+            continue
         
+        if pm.target:
+            p = pm.target 
+            pm.point_target = None
+            print(f"MOVE to pellet at {p.pos.x} {p.pos.y} with value {p.value} at dist { math.dist(p.pos, pm.pos) }", file=sys.stderr, flush=True)
+            output.append(f"MOVE {pm._id} {p.pos.x} {p.pos.y}")
+            continue
+        
+        if not pm.point_target:            
+            pm.point_target = random.choice( open_points )
+
+        pt = pm.point_target
+        print(f"MOVE to random point at {pt.x} {pt.y}", file=sys.stderr, flush=True)
+        output.append(f"MOVE {pm._id} {pt.x} {pt.y}")
+        continue
+
+
     print(" | ".join( output))
 
     # Write an action using print
